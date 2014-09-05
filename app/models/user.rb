a331941ccbe880f75c42dfdeafadb4c188557282
Mike Murphy <mikeymurph77@gmail.com>
2014-09-04 21:47:55 -0400

    Add username and additional user information
    A user can now login with a username or email.
    Created user profile page which display basic information about the
    user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :login
+
+  validates :username, uniqueness: { case_sensitive: false }
+
+  before_save do
+    self.username.downcase if self.username
+    self.email.downcase if self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { value: login }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  def twitter_handle
+    twitter[1..-1]
+  end
+
+  def twitter_link
+    "https://twitter.com/#{twitter_handle}"
+  end
 end
