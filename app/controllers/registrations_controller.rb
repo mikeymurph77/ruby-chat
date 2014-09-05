d5f60eb997501ce8f3d91d1741cfecfe3b7a3ba8
Mike Murphy <mikeymurph77@gmail.com>
2014-09-05 13:57:02 -0400

    Fix formatting in registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,10 +2,35 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :username, :twitter, :linkedin, :github, :blog, :bio)
+    params.require(:user).permit(
+      :first_name, 
+      :last_name, 
+      :email, 
+      :password, 
+      :password_confirmation, 
+      :username, 
+      :twitter, 
+      :linkedin, 
+      :github, 
+      :blog, 
+      :bio
+    )
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :username, :twitter, :linkedin, :github, :blog, :bio)
+    params.require(:user).permit(
+      :first_name, 
+      :last_name, 
+      :email, 
+      :password, 
+      :password_confirmation, 
+      :current_password, 
+      :username, 
+      :twitter, 
+      :linkedin, 
+      :github, 
+      :blog, 
+      :bio
+    )
   end
 end
