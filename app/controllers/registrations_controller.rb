27875980048d2da1e9af7ec950572de3a50e5933
Mike Murphy <mikeymurph77@gmail.com>
2014-09-05 13:58:51 -0400

    Fix trailing whitespace in registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,33 +3,33 @@ class RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
     params.require(:user).permit(
-      :first_name, 
-      :last_name, 
-      :email, 
-      :password, 
-      :password_confirmation, 
-      :username, 
-      :twitter, 
-      :linkedin, 
-      :github, 
-      :blog, 
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :username,
+      :twitter,
+      :linkedin,
+      :github,
+      :blog,
       :bio
     )
   end
 
   def account_update_params
     params.require(:user).permit(
-      :first_name, 
-      :last_name, 
-      :email, 
-      :password, 
-      :password_confirmation, 
-      :current_password, 
-      :username, 
-      :twitter, 
-      :linkedin, 
-      :github, 
-      :blog, 
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :current_password,
+      :username,
+      :twitter,
+      :linkedin,
+      :github,
+      :blog,
       :bio
     )
   end
