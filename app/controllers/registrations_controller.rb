7dfd68ec83ae637704d563b16fa065ed392ab312
Mike Murphy <mikeymurph77@gmail.com>
2014-09-05 13:52:42 -0400

    Fix whitespace

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   private
- 
+
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :username, :twitter, :linkedin, :github, :blog, :bio)
   end
- 
+
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :username, :twitter, :linkedin, :github, :blog, :bio)
   end
