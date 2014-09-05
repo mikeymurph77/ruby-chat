a6737059a2821cf49135a676c5a3aad2e844ef59
Mike Murphy <mikeymurph77@gmail.com>
2014-09-05 14:53:57 -0400

    make username downcase again
    Was done on previous commit....forgot to save

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,14 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: { case_sensitive: false }
 
   before_save do
-    self.username.downcase if self.username
-    self.email.downcase if self.email
+    self.username.downcase! if self.username
+    self.email.downcase! if self.email
   end
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { value: login }]).first
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { value: login.downcase }]).first
     else
       where(conditions).first
     end
